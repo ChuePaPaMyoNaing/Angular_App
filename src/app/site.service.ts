d4fcd1c2911a94544e77e3b8b9326e1bd2d565ef
chuepapamyonaing <chuepapamyonaing@cybermissions.co.jp>
2022-11-25 11:41:12 +0900

    HttpClientによるCRUD操作

---
+++ src/app/site.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Site } from './site.model';
+import { Observable } from 'rxjs';
+
+const apiUrl = 'http://localhost:3000/sites';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteService {
+
+  constructor(private http: HttpClient) { }
+
+  getSites():Observable<Site[]>{
+    return this.http.get<Site[]>(apiUrl);
+  }
+
+  createSite(siteInfo: Site):Observable<Site> {
+    const body = JSON.stringify(siteInfo);
+    return this.http.post<Site>(apiUrl, body, httpOptions);
+  }
+
+  updateSite(id: string, siteInfo: Site): Observable<Site> {
+    const body = JSON.stringify(siteInfo);
+    return this.http.put<Site>(`${apiUrl}/${id}`, body, httpOptions);
+  }
+
+  deleteSite(id: string) {
+    return this.http.delete(`${apiUrl}/${id}`);
+  }
+}
